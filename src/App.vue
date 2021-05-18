707d1a52f219ed93cbce403aa246dd927a18ab8f
xiexiaoyu <xiexiaoyu@piesat.cn>
2021-05-18 17:06:04 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,18 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Provide, Vue } from "vue-property-decorator";
 
 @Component({})
 export default class app extends Vue {
   $http: any;
+  list: any;
+  a: any;
+  
+  @Provide() b: any = true;
   private data() {
     return {
+      a: 1,
       list: [
         {
           name: "Home",
@@ -33,21 +38,28 @@ export default class app extends Vue {
       ],
     };
   }
+  private test(a: any) {
+    console.log(a);
+  }
+  // 生命周期
   private mounted() {
+    let a: any = "hhhhhhhhhh";
+    this.test(a);
+    console.log(this.a);
+    console.log(this.list);
     let path = "http://localhost:8080/api/v1/gateway_manager/test";
     let params = { a: 1, b: 2 };
     this.$http.get(path, params).then((res: any) => {
       console.log(res);
     });
     let path2 = "http://localhost:8080/api/v1/gateway_manager/testPost";
-    let params2 = { name:"xiaoyu" };
+    let params2 = { name: "xiaoyu" };
     this.$http.post(path2, params2).then((res: any) => {
       console.log(res);
     });
   }
 }
 </script>
-
 
 <style lang="scss">
 #app {
